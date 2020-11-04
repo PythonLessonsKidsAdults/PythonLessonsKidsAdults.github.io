549f1bd9d3bd8efe33fe491b62e6ee4b37888579
K1ngAng31 <angelsantanahernandez2@gmail.com>
2020-11-03 23:17:08 -0700

    Teaching Python

---
+++ Lesson1.py
@@ -0,0 +1,63 @@
+def printStatements():
+    '''
+    Please fill in where asked
+    NO NEED FOR VARIABLES
+    '''
+    #Create a print an empty string
+
+    #--------------------------------------
+    #print the string and int version of 100
+
+    #--------------------------------------
+    #print a simple addition equation
+    #no variables
+
+    #--------------------------------------
+
+
+def simpleMath():
+    '''
+    PLEASE FILL IN WHERE ASKED
+    --------------------------
+    Operators: +, -, /, *
+    '''
+    # CREATE 4 VARIABLES WITH DIFFERENT NUMBERS
+    var1 = #replace this with a number
+    var2 = #replace this with a number
+    var3 = #replace this with a number
+    var4 = #replace this with a number
+
+    #--------------------------------------
+    # ADD THE 4 NUMBERS TOGETHER
+    result_add = 0 #use this to put the result
+    #--------------------------------------
+    # SUBTRACT THE 4 NUMBERS
+    result_subtract = 0 #use this to put the result
+
+    #--------------------------------------
+    #DIVIDE THE 4 NUMBERS
+    result_divide = 0 #use this to put the result
+
+    #--------------------------------------
+    # MULTIPLY THE 4 NUMBERS
+    result_multiply = 0 #use this to put the result
+
+    #--------------------------------------
+    # DO NOT MODIFY THIS
+    print("Adding %s, %s, %s, %s, gives us: %s "%(var1, var2, var3, var4, result_add))
+    print("Subtracting %s, %s, %s, %s, gives us: %s"%(var1, var2, var3, var4, result_subtract))
+    print("Dividing %s, %s, %s, %s, gives us: %s"%(var1, var2, var3, var4, result_divide))
+    print("Multiplying %s, %s, %s, %s, gives us: %s"%(var1, var2, var3, var4, result_multiply))
+
+
+def fixMath():
+    '''
+    PLEASE FIX THE FOLLOWING
+    '''
+
+
+
+#DO NOT MODIFY THE BOTTOM
+if __name__ == '__main__':
+    printStatements()
+    simpleMath()
